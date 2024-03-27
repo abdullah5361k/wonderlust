a727a774b0904103a74c3ef09376890c5a4ef0c6
abdullah5361k <abdullah5361k@gmail.com>
2024-03-27 12:24:20 +0500

    Added Atlas url

--- Backend/config/db.config.js
+++ Backend/config/db.config.js
@@ -2,8 +2,7 @@ const mongoose = require("mongoose");
 
 const dbConnection = async () => {
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/airbnb');
-        // await mongoose.connect('mongodb://localhost/airbnb');
+        await mongoose.connect(process.env.DB_URL);
         console.log("Connection successfull")
     } catch (error) {
         console.log("Db Error ", error)
